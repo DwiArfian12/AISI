f9631a0ee5c8bf6f41f20c91cf75b6bc8c7d8f05
DwiArfian12 <dwiarfiann@gmail.com>
2023-04-05 17:35:05 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function toNormal() {
+    location.href = '/normal/index.html'
+}
+function toDisable() {
+    location.href = '/disable/index.html'
+}
+
+const navbar = document.getElementById('navbar');
+window.onscroll = () => {
+    if (window.scrollY > 150) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
