bc5a448f7b4e58a2041adbaf9a112adc3e3f3959
DwiArfian12 <dwiarfiann@gmail.com>
2023-04-06 21:18:36 +0700

    add courses

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,19 @@ function toDisable() {
     location.href = 'disable/index.html'
 }
 
+function enterKeyPressed(event) {
+    if (event.keyCode == 49) {
+       location.href = 'normal/index.html'
+    } else if (event.keyCode == 50) {
+      location.href = 'disable/index.html'
+    } 
+ }
+function back(event) {
+    if (event.keyCode == 48) {
+       location.href = '/index.html'
+    }
+ }
+
 const navbar = document.getElementById('navbar');
 window.onscroll = () => {
     if (window.scrollY > 150) {
